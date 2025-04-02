258cca08ddfdaeb647411232e2d18dfe3ee3019f
MichalFijak <michalfijak1@hotmail.com>
2025-04-02 20:05:30 +0200

    exceptions

--- poop/main.py
+++ poop/main.py
@@ -19,3 +19,22 @@ def get_ice_cream(flavor):
     print(f"Here's your {flavor} ice cream !")
 
 get_ice_cream("vanilla")
+
+
+
+# exceptions
+
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError as e:
+        print("You can't divide by zero!")
+    except TypeError as e:
+        print("Invalid type!")
+    finally:
+        print("Execution finished!")
+
+print(divide(10, 0))
+print(divide(10, "something"))
+print(divide(10, 2))
+
